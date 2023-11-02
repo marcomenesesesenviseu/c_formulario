4edbef8086af2bc97319c7e633b8d5b984ff7a0d
Marco Meneses <emes3soft@gmail.com>
2023-11-02 11:17:21 +0000

    Add files via upload

--- exemplo/index.js
+++ exemplo/index.js
@@ -5,15 +5,22 @@ const f_Login = new c_formulario("frmLogin");
 // Adição das validações ao formulário:
 f_Login.Adiciona_Validacoes(["txtUtilizador","«txtUtilizador» == ''", "* Obrigatório"]);
 f_Login.Adiciona_Validacoes(["txtUtilizador","«txtUtilizador.indexof(',')» > -1", "* O nome do utilizador não pode conter o carater ,!"]);
-f_Login.Adiciona_Validacoes(["txtPalavraPasse","«txtPalavraPasse» < 6", "* A palavra-passe tem de ter pelo menos 10 digitos"]);
+f_Login.Adiciona_Validacoes(["txtUtilizador","!«txtUtilizador.email»", "* Endereço de correio eletrónico inválido!"]);
+f_Login.Adiciona_Validacoes(["txtPalavraPasse","«txtPalavraPasse.len» < 10", "* A palavra-passe tem de ter pelo menos 10 digitos"]);
 f_Login.Adiciona_Validacoes(["txtPalavraPasse","«txtPalavraPasse.includes(';')»", "* A palavra-passe contém carateres inválidos!"]);
-f_Login.Adiciona_Validacoes(["chkPolitica","!«chkPolitica.checeked»", "* É obrigatório concordar com as políticas para entrar!"]);
+f_Login.Adiciona_Validacoes(["chkPolitica","!«chkPolitica.checked»", "* É obrigatório concordar com as políticas para entrar!"]);
 
 // Teste das validações ao submeter o formulário
 
 $(document).ready(function() {
-    $("#frmLogin").on("submit", function() {
+    $("#frmLogin").on("submit", function(e) {
+        e.preventDefault();
         if (!f_Login.Testa_Validacoes())
             alert('O formulário será enviado!');
+
+        // Numa situação real o código js seria
+        
+        // if (f_Login.Testa_Validacoes())
+        //     e.preventDefault();
     });
 });
