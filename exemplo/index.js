446c46ecb1ea5e1d35c791dcca545ef49e08c0e1
Marco Meneses <emes3soft@gmail.com>
2023-11-12 11:13:00 +0000

    Add files via upload

--- exemplo/index.js
+++ exemplo/index.js
@@ -1,9 +1,10 @@
+// Exemplo da aplicação do C_FORMULÁRIO - Validador de formulários
 
 // Associa o formulário à class c_formulario
 const f_Login = new c_formulario("frmLogin");
 
 // Adição das validações ao formulário:
-f_Login.Adiciona_Validacoes(["txtUtilizador","«txtUtilizador» == ''", "* Obrigatório"]);
+f_Login.Adiciona_Validacoes(["txtUtilizador","«txtUtilizador.val» == ''", "* Obrigatório"]);
 f_Login.Adiciona_Validacoes(["txtUtilizador","«txtUtilizador.indexof(',')» > -1", "* O nome do utilizador não pode conter o carater ,!"]);
 f_Login.Adiciona_Validacoes(["txtUtilizador","!«txtUtilizador.email»", "* Endereço de correio eletrónico inválido!"]);
 f_Login.Adiciona_Validacoes(["txtPalavraPasse","«txtPalavraPasse.len» < 10", "* A palavra-passe tem de ter pelo menos 10 digitos"]);
@@ -13,6 +14,8 @@ f_Login.Adiciona_Validacoes(["chkPolitica","!«chkPolitica.checked»", "* É obr
 // Teste das validações ao submeter o formulário
 
 $(document).ready(function() {
+    $("body").css("margin-top","50px");
+
     $("#frmLogin").on("submit", function(e) {
         e.preventDefault();
         if (!f_Login.Testa_Validacoes())
@@ -22,5 +25,10 @@ $(document).ready(function() {
         
         // if (f_Login.Testa_Validacoes())
         //     e.preventDefault();
+    });
+
+    $("#butLimpar").on("click", function() {
+        $("#frmLogin").trigger("reset");
+        $("#txtUtilizador").focus();
     });
 });
